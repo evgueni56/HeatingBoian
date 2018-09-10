4a909ed5d664f11549f2bb51347445fd902c4b69
Evgueni Nikolov <evgueni.nikolov@outlook.com>
2018-09-10 15:11:05 +0300

    Initial commit

---
+++ Release/Time/DateStrings.cpp.d
@@ -0,0 +1,4 @@
+C:\Users\evgueni\ nikolov\AppData\Local\Temp\VMBuilds\HeatingBoian\esp8266_generic\Release\Time\DateStrings.cpp.o: \
+ C:\User\OneDrive\Arduino\libraries\Time\DateStrings.cpp \
+ C:\User\OneDrive\Arduino\libraries\Time\Time.h \
+ C:\User\OneDrive\Arduino\libraries\Time\TimeLib.h
